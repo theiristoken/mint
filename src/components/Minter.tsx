a164c7188e0bc0dec090ab517157a8b73fab146a
na-ha <nadirhajarabi@gmail.com>
2023-08-14 17:59:13 +0200

    Update Minter.tsx

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -192,7 +192,7 @@ export default function Minter({time, out}: {time: number, out: boolean}) {
 			setAmount(Number(signature.payload.quantity));
 			console.log(signature, Number(signature.payload.quantity));
 			setIsClaiming(false);
-			
+			setClaimed(true);
 		} else {
 			setIsClaiming(false);
 			setErrorShown(true);
@@ -232,7 +232,7 @@ export default function Minter({time, out}: {time: number, out: boolean}) {
 			{(isLoading || isLoadingToken) && <Loader/>}
 
 			{!verified && !isLoading && !isLoadingToken && address && 
-			<div className="flex flex-col items-center justify-center p-4 self-center">
+			<div className="flex flex-col items-center justify-center py-4 self-center">
 				<p className="text-md text-center mt-2">Only iris Verified individuals (iVIs) can claim TiTs.</p>
 				<IDKitWidget
 					action={Constants.wldAction}
@@ -264,7 +264,7 @@ export default function Minter({time, out}: {time: number, out: boolean}) {
 					
 				</div>
 				
-				{isMismatched && <div className="flex flex-col items-center justify-center p-4 self-center">
+				{isMismatched && <div className="flex flex-col items-center justify-center py-4 self-center">
 					<button  
 						disabled={!isMismatched}
 						onClick={()=>switchNetwork(activeChain)}
@@ -274,8 +274,8 @@ export default function Minter({time, out}: {time: number, out: boolean}) {
 					<p className="flex flex-col items-center justify-center self-center text-md font-bold text-rose-500 mt-2">TiTs are on the Optimism chain.</p>
 				</div>}	
 
-				{!minted && !claimed && time<=0 && !out && !isMismatched && !isClaiming && <div className="flex flex-col items-center justify-center p-4 self-center">
-					<p className="flex flex-col items-center justify-center self-center text-md text-center mt-4">You may claim {evaluate(-time)}.</p>
+				{!minted && !claimed && time<=0 && !out && !isMismatched && !isClaiming && <div className="flex flex-col items-center justify-center py-4 self-center">
+					<p className="flex flex-col items-center justify-center self-center text-md text-center mt-2">You may claim {evaluate(-time)}.</p>
 					<button 
 						disabled={isLoadingToken || isMismatched || minted || isClaiming || time>=0}
 						onClick={onClaim} 
@@ -300,11 +300,11 @@ export default function Minter({time, out}: {time: number, out: boolean}) {
 					<a className="font-bold hover:text-neutral-200 text-slate-600 " href="https://twitter.com/theiristoken" target="_blank">Need gas money?</a>
 				</div>}	
 
-				{!isMismatched && (isClaiming || isMinting) && <div className="flex flex-col items-center justify-center p-4 self-center">
+				{!isMismatched && (isClaiming || isMinting) && <div className="flex flex-col items-center justify-center py-4 self-center">
 					<Loader/>
 				</div>}	
 				
-				{minted && claimed && !newMint && !isMismatched && <div className="flex flex-col items-center justify-center p-4 self-center">
+				{minted && claimed && !newMint && !isMismatched && <div className="flex flex-col items-center justify-center py-4 self-center">
 				<div className="flex flex-row items-center justify-center px-2 self-center">
 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" className="w-6 h-6 stroke-pink-500 hover:fill-white mr-1">
 						<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
@@ -324,7 +324,7 @@ export default function Minter({time, out}: {time: number, out: boolean}) {
 					{isLoadingBalance && <Loader/>}
 				</div>}	
 
-				{minted && claimed && newMint && !isMismatched && <div className="flex flex-col items-center justify-center p-4 self-center">
+				{minted && claimed && newMint && !isMismatched && <div className="flex flex-col items-center justify-center py-4 self-center">
 					<div className="flex flex-row items-center justify-center px-2 self-center">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" className="w-6 h-6 stroke-pink-500 hover:fill-white mr-1">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
