1490c7f5350988c310d332282e51017ecdb2a8b5
na-ha <nadirhajarabi@gmail.com>
2023-08-20 14:31:01 +0200

    iOS font fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,27 @@ import { ThirdwebProvider, metamaskWallet } from '@thirdweb-dev/react';
 import type { AppProps } from 'next/app'
 import Head from 'next/head';
 import Constants from "@/constants";
+import { useEffect } from 'react';
 
 const clientId = Constants.clientId;
 const activeChain = Constants.activeChain;
 
 export default function App({ Component, pageProps }: AppProps) {
+  
+  useEffect(()=>{
+    const userAgent = navigator.userAgent;
+    const versionString = userAgent.match(/OS ((\d+_?){2,3})\s/);
+    if (versionString && versionString[1]){
+      const version = Number(versionString[1].replace("_","."));
+      if(version>=16) {
+        document.body.style.fontFamily = 'Outfit';
+      }
+    } else {
+      document.body.style.fontFamily = 'Cairo Play';
+    }
+  }, [])
+
+
   return (
     <ThirdwebProvider 
       clientId={clientId}
@@ -23,7 +39,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Cairo+Play:wght@300;600&display=swap" rel="preload"/>
+        <link href="https://fonts.googleapis.com/css2?family=Cairo+Play:wght@300;600&family=Outfit:wght@300;600&display=swap" rel="stylesheet"/>
         <meta
           name="keywords"
           content="Nadir Hajarabi, crypto, cryptocurrency, digital asset, collateral, defi, blockchain, ico, token, worldcoin, iris scan, world id, metamask, orb, orb verification, eye scan, biometrics, proof of personhood"
